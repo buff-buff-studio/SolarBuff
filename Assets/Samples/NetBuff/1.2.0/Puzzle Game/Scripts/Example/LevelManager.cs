6f40977bc9bbd5b9d1d938422c0194ba532190a8
Arnilsen Arthur <arnilsenarthur@gmail.com>
2024-03-10 14:11:07 -0300

    Fix

--- Assets/Samples/NetBuff/1.2.0/Puzzle Game/Scripts/Example/LevelManager.cs
+++ Assets/Samples/NetBuff/1.2.0/Puzzle Game/Scripts/Example/LevelManager.cs
@@ -2,6 +2,7 @@
 using NetBuff.Components;
 using NetBuff.Misc;
 using SolarBuff.Data;
+using UnityEngine;
 
 namespace ExamplePlatformer
 {
@@ -21,23 +22,39 @@ namespace ExamplePlatformer
             }
             private set => _instance = value;
         }
-        
+
         public IntNetworkValue levelIndex = new IntNetworkValue(0, NetworkValue.ModifierType.Server);
         public NetworkIdentity[] levels;
 
         private void OnEnable()
         {
+            var profile = SaveManager.GetCurrentProfile();
+            if (profile.Body.ContainsKey("level"))
+                levelIndex = new IntNetworkValue(profile.Body["level"], NetworkValue.ModifierType.Server);
+
             WithValues(levelIndex);
+            levelIndex.OnValueChanged += OnChangeLevel;
+        }
+
+        private void OnChangeLevel(int oldValue, int newValue)
+        {
+            var profile = SaveManager.GetCurrentProfile();
+            Debug.Log("Changing level from " + oldValue + " to " + newValue);
+            profile.Body["level"] = newValue;
+            #pragma warning disable CS4014
+            SaveManager.Save();
+            #pragma warning restore CS4014
+            
+            if (!HasAuthority)
+                return;
+            levels[oldValue].SetActive(false);
+            levels[newValue].SetActive(true);
         }
 
         public override void OnSpawned(bool isRetroactive)
         {
             if (!HasAuthority) 
                 return;
-            
-            var profile = SaveManager.GetCurrentProfile();
-            if (profile.Body.ContainsKey("level"))
-                levelIndex.Value = profile.Body["level"];
             
             Instance = this;
             levels[levelIndex.Value].SetActive(true);
@@ -47,15 +64,8 @@ namespace ExamplePlatformer
         {
             if (!HasAuthority)
                 return;
-            levels[levelIndex.Value].SetActive(false);
-            levelIndex.Value = level;
-            levels[levelIndex.Value].SetActive(true);
             
-            var profile = SaveManager.GetCurrentProfile();
-            profile.Body["level"] = levelIndex.Value;
-            #pragma warning disable CS4014
-            SaveManager.Save();
-            #pragma warning restore CS4014
+            levelIndex.Value = level;
         }
     }
 }
