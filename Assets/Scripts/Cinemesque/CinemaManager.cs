6748442e8553eba42554db9dc153405498b78160
Arnilsen Arthur <arnilsenarthur@gmail.com>
2024-03-19 16:44:24 -0300

    First Commit

---
+++ Assets/Scripts/Cinemesque/CinemaManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TMPro;
+
+namespace SolarBuff.Cinemesque
+{
+    public class CinemaManager : SingletonBehaviour<CinemaManager>
+    {
+        /// <summary>
+        /// Used to display dialog
+        /// </summary>
+        public TMP_Text labelDialog;
+        
+        /// <summary>
+        /// Used to display any other subtitles
+        /// </summary>
+        public TMP_Text labelExternal;
+        
+        [Serializable]
+        public class Text
+        {
+            public int slot;
+            public float duration = 0f;
+            public string text;
+        }
+    }
+}
